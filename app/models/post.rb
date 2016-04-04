24784b91eef7a667ac197b70537f7e12f1fe5b7f
arnaud huguenin <hhuguenin.arnaud@gmail.Com>
2016-04-04 17:16:58 +0200

    model post & show updated

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ActiveRecord::Base
 
   has_many :favs, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+  # CATEGORIES = ["INTERVIEW","ARTICLE", "T8PE", "VIDEO"]
+  # SOUS_CATEGORIES = ["CLASH", "RAP", "LOFI", "BEATS", "TRAP", "ARTISTE", "HOW TO MAKE IT", "HOW IT'S DONE", "TUTO"]
+
+  # validates :user_id, presence: true
 end
