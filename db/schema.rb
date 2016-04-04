47b61e326e3f7ca55fafcbafd5701002b4bcd83e
arnaud huguenin <hhuguenin.arnaud@gmail.Com>
2016-04-04 22:29:12 +0200

    init cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404164106) do
+ActiveRecord::Schema.define(version: 20160404202610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160404164106) do
     t.string   "hashtag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "photo"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
