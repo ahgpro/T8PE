6d031b7128bd01dd44f4dd81572226c75e82b1a4
arnaud huguenin <hhuguenin.arnaud@gmail.Com>
2016-04-08 16:57:23 +0200

    mailer user fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,2 @@
 class UsersController < ApplicationController
-	def create
-    @user = current_user.users.build(user_params)
-
-    if @user.save
-      UserMailer.creation_confirmation(@user).deliver_now
-      redirect_to user_path(@user)
-    else
-      render :new
-    end
-  end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,11 +6,9 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.welcome.subject
   #
   def welcome(user)
-    @user = user
+    @user = user  # Instance variable => available in view
 
-    mail(
-      to:       @user.email,
-      subject:  "user #{@user.email} created!"
-    )
+    mail(to: @user.email, subject: 'Welcome to LeWagon')
+    # This will render a view in `app/views/user_mailer`!
   end
 end
