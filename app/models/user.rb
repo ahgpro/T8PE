3eef3e016ebb721bdf9bfb55d014367319243a71
arnaud huguenin <hhuguenin.arnaud@gmail.Com>
2016-04-11 16:21:39 +0200

    implement a FIND_FOR_FACEBOOK_OAUTH

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,20 @@ class User < ActiveRecord::Base
   has_many :favs, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
+  def self.find_for_facebook_oauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.picture = auth.info.image
+      user.token = auth.credentials.token
+      user.token_expiry = Time.at(auth.credentials.expires_at)
+    end
+  end
+
   # def admin?
   #     user = user.find(params[:id])
   #     user.admin = true
